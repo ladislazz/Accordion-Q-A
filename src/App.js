d26b82f7db9f65bf284c5504dd283f2fe66f474a
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-31 00:22:13 -0400

    Remove question destructuring. Include useState

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <main>
       <div className="container">
         <h3>questions and answers about login</h3>
         <section className="info">
-          {data.map((question) => {
-            const { id, title, info } = question;
-            return <SingleQuestion key={id} title={title} info={info} />;
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
           })}
         </section>
       </div>
