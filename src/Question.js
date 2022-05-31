b652d737af7be4328e43f944b4f0e85a53dfa63f
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-30 23:56:40 -0400

    Configure buttons. Show/hide par feature

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ id, title, info }) => {
+  const [showPar, setShowPar] = useState(false);
+
+  const btnClick = () => {
+    setShowPar(!showPar);
+  };
+
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">
-          <AiOutlineMinus />
+        <button className="btn" onClick={btnClick}>
+          {showPar ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      <p>{info}</p>
+      {showPar && <p>{info}</p>}
     </article>
   );
 };
