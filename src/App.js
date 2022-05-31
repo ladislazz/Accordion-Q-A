ba1c8981e6508b1dea16828c5a76efd2930bbc74
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-30 23:39:20 -0400

    Display SingleQuestion components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {data.map((question) => {
+            const { id, title, info } = question;
+            return <SingleQuestion key={id} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
